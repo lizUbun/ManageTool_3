5391e38219929cdaf4898a3f16aa7c639d2b6b77
lizzyU <1255621972@qq.cn>
2017-04-02 16:21:30 +0800

    add the give back layout and basic give back record format

--- app/src/main/java/com/xlc/managetool_3/BorrowListAcivity.java
+++ app/src/main/java/com/xlc/managetool_3/BorrowListAcivity.java
@@ -1,13 +1,30 @@
 package com.xlc.managetool_3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BorrowListAcivity extends AppCompatActivity {
+    Button borrowBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrow_list_acivity);
+        //
+        initView();
+        borrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BorrowListAcivity.this,ConfirmBack.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        borrowBack = (Button)findViewById(R.id.borrow_back_button);
     }
 }
