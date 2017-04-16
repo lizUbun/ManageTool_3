67611aa5becdbfb352fcc8b72d67a9eb536f47a1
lizzyU <1255621972@qq.cn>
2017-04-16 09:01:38 +0800

    use the drawable background way to deal the click and change color operation 使用drawable的方式改变背景，解决了点击事件，选择工具

---
+++ app/src/main/java/com/xlc/managetool_3/AllDataControl.java
@@ -0,0 +1,68 @@
+package com.xlc.managetool_3;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
+
+import com.xlc.tool.ManageTool;
+
+import java.util.ArrayList;
+
+public class AllDataControl extends Service {
+    Handler handler;
+
+    // define the statement of the data
+    //  定义当前数据显示的状态
+    public static int data_state = 0;
+    public static final int DATA_STATE_INIT = 1;
+
+    // define the display string
+    // 定义需要显示的工具条目
+    public static ArrayList<String> display;
+
+    // look list activity selected items
+    // 在挑选工具中被选中的工具
+    public static ArrayList<Integer> selected_tools;
+
+    public AllDataControl() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        handler = new Handler() {
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                // if get the flush data message
+
+                if (msg.what == ManageTool.FLUSH_DATA) {
+
+                }
+            }
+
+        };
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        handler = new Handler() {
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                // if get the flush data message
+                if (msg.what == ManageTool.FLUSH_DATA) {
+
+                }
+            }
+        };
+        return null;
+    }
+}
